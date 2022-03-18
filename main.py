5cca987ec6fdd3801233cf8a7079a74e1163e994
DEV-VASU <101116280+DEV-VASU@users.noreply.github.com>
2022-03-18 22:11:37 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ from pyrogram.types import (InlineQueryResultArticle, InputTextMessageContent,
 
 app = Client(
     "Pyrogram Bot",
-    bot_token = os.environ["BOT_TOKEN"],
-    api_id = int(os.environ["API_ID"]),
-    api_hash = os.environ["API_HASH"],
+    bot_token = "5192168337:AAGzHN3Yx8xcg8Ou5vDa830GejloyXKDtFc",
+    api_id = 3024532,
+    api_hash = "370817294c7a66e888fd06fa264f6dbd",
 )
 
 @app.on_inline_query()
