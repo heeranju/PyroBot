643ebc5a0300a4bf121f6b53e288924b63b4d67f
DEV-VASU <101116280+DEV-VASU@users.noreply.github.com>
2022-03-19 08:52:22 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,40 +9,6 @@ app = Client(
     api_hash = "370817294c7a66e888fd06fa264f6dbd",
 )
 
-home_keyboard_pm = InlineKeyboardMarkup(
-    [
-        [
-            InlineKeyboardButton(
-                text="Commands ❓", url=f"http://t.me/{BOT_USERNAME}?startgroup=new"
-            ),
-            InlineKeyboardButton(
-                text="👤 Bot Owner",
-                user_id = 1926090919,
-            ),
-        ],
-        [
-            InlineKeyboardButton(
-                text="System Stats 🖥",
-                url=f"http://t.me/{BOT_USERNAME}?startgroup=new",
-            ),
-            InlineKeyboardButton(
-                text="🔎 Inline", switch_inline_query_current_chat = ''
-            ),
-        ],
-        [
-            InlineKeyboardButton(
-                text="✚ Add Me To Your Group",
-                url=f"http://t.me/{BOT_USERNAME}?startgroup=new",
-            )
-        ],
-    ]
-)
-
-home_text_pm = (
-    f"Hey there! My name is {BOT_NAME}. I can manage your "
-    + "group with lots of useful features, feel free to "
-    + "add me to your group."
-)
 
 
 @app.on_message(filters.text & filters.private)
